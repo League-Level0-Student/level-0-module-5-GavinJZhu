0c006d82bd1e453eee36bf894fd5df0d7eadadba
Gavin <gzhu100@icloud.com>
2022-11-12 11:29:17 -0800

    finished fibonacci and goofy names,working on Snowflake.java

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -4,36 +4,42 @@ package _01_algorithms._3_goofy_names;
  *    Level 0
  */
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class GoofyNames {
-	public static void main(String[] args) {
-		String goofyName = "";
+    public static void main(String[] args) {
+        String goofyName = "";
 
-		// 1. Ask the user to enter their name
+        // 1. Ask the user to enter their name
+        String name = JOptionPane.showInputDialog("What's your name?");
+        // 2. Print upper case name to the console using .toUpperCase()
+        //    Run your program to see that this works.
+        System.out.println(name.toUpperCase());
+        // 3. Loop through each character of the name (steps 4 - 7).
+        //    HINT: Use .length() to determine the number of characters in the String.
+        for (int i = 0; i < name.length(); i++) {
 
-		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
 
-		// 3. Loop through each character of the name (steps 4 - 7).
-		//    HINT: Use .length() to determine the number of characters in the String.
+            // 4. Create a char variable to store the next character of the name
+            //    use .charAt()
+            char letter = name.charAt(i);
 
-				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
-	
-				
-				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
-				// 6. Even characters should be made uppercase and odd characters made lowercase
-				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
-				// 7. ADD the char to the end of the goofyName String
+            // 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+            if (letter % 2 == 0) {
+                letter = Character.toUpperCase(letter);
+            } else {
+                letter = Character.toLowerCase(letter);
+            }
 
-		
-		// 8. Use pop-up to show user their Goofy name
+            // 6. Even characters should be made uppercase and odd characters made lowercase
+            //    HINT: use Character.toUppercase() or Character.toLowerCase()
 
-	}
+
+            // 7. ADD the char to the end of the goofyName String
+			goofyName = goofyName+letter;
+        }
+        // 8. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null,goofyName);
+    }
 }
 
