9a5989241010e752435a72431a38448729df8562
Gavin <gzhu100@icloud.com>
2022-11-19 11:26:57 -0800

    working on for loop gauntlet

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/for_loop_gauntlet.java
@@ -0,0 +1,28 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class for_loop_gauntlet {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j<3; j++){
+                System.out.println(i+" "+j);
+            }
+        }
+        for (int a = 1; a<10; a++){
+            System.out.print(a+ " ");
+            if (a % 3 == 0){
+                System.out.println();
+            }
+        }
+        for (int b = 1; b<101; b++){
+            System.out.print(b+ " ");
+            if (b % 10 == 0){
+                System.out.println();
+            }
+        }
+        for (int c = 1; c<7; c++){
+          //  for (int d = 0; d<)
+        }
+
+    }
+}
+
